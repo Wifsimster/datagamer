d10aa847904a04ede22550c87310fb0fd3ef97fb
Wifsimster <wifsimster@gmail.com>
2015-01-30 23:52:56 +0100

    Start settings menu

--- public/js/demo.js
+++ public/js/demo.js
@@ -2,11 +2,8 @@ var app = angular.module('StarterApp', ['ngMaterial']);
 
 app.controller('AppCtrl', ['$scope', '$mdSidenav', function ($scope, $mdSidenav) {
 
-    $scope.data = {
-        selectedIndex: 0,
-        secondLocked: true,
-        secondLabel: "Item Two"
-    };
+    $scope.settings = ["General", "Searcher", "Downloaders", "Renamer", "Automation", "Notifications", "Manage", "About"];
+
     $scope.next = function () {
         $scope.data.selectedIndex = Math.min($scope.data.selectedIndex + 1, 2);
     };
