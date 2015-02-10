5f5b0897974465904c048471a28a8082482a2c52
Wifsimster <wifsimster@gmail.com>
2015-02-10 11:17:22 +0100

    Implement TPB provider binding with Transmission.

--- app/routes/thepiratebay.js
+++ app/routes/thepiratebay.js
@@ -19,11 +19,11 @@ app.get("/thepiratebay/test", function (req, res) {
 
 
 // Example :
-//  name: 'Far.Cry.4.[v1.5].Repack-R.G.Mechanics',
+//    name: 'Far.Cry.4.[v1.5].Repack-R.G.Mechanics',
 //    size: '13.34 GiB',
 //    link: 'http://thepiratebay.se/torrent/11668819/Far.Cry.4.[v1.5].Repack-R.G.Mechanics',
 //    category: { id: '400', name: 'Games' },
-//  seeders: '3610',
+//    seeders: '3610',
 //    leechers: '380',
 //    uploadDate: '12-09 2014',
 //    magnetLink: 'magnet:?xt=urn:btih:0da12a42ef5aca2b75dc',
@@ -39,29 +39,82 @@ app.get("/thepiratebay/search/:name", function (req, res) {
 
     // Search a tracker in game category
     tpb.search(name, {category: '400'})
-        .then(function (results) {
+        .then(function (torrents) {
 
-            var result;
+            // The torrent send to transmission if it passed the filter
+            var favorite_torrent = null;
 
-            for(var i = 0 ; i < results.length ; i++) {
+            for (var i = 0; i < torrents.length; i++) {
+
+                var torrent = torrents[i];
+
+                // Set the favorite torrent as current before filters
+                favorite_torrent = torrent;
+
                 // First, only take PC game tracker
-                if(results[i].subcategory.id == '401') {
-                    // Filter white list
+                if (torrent.subcategory.id == '401') {
 
-                    // Filter black list
+                    console.log("Torrent subject to be taken : " + torrent.name);
 
-                    // Filter size
+                    // Filter white list of words
+                    if (config.thepiratebay.filters.favorite_words) {
+                        var words = config.thepiratebay.filters.favorite_words.split(',');
+                        for (var i = 0; i < words.length; i++) {
+                            // TODO : Check in the torrent name if the word exist, if so keep this torrent and go on
+                        }
+                    }
 
-                    // Filter seeders
+                    // Filter black list of words
+                    if (config.thepiratebay.filters.forbidden_words) {
+                        var words = config.thepiratebay.filters.forbidden_words.split(',');
+                        for (var i = 0; i < words.length; i++) {
+                            // TODO : Check in the torrent name if the word exist, if so reject this torrent and go to the next one
+                        }
+                    }
 
-                    // Filter leechers
+                    // Filter size : If torrent size <= config size reject the torrent
+                    if (torrent.size <= config.thepiratebay.filters.size_min) {
+                        console.log("-- Torrent reject : Size too small (" + torrent.size + ")");
+                        favorite_torrent = null;
+                    }
 
-                    // Filter uploadDate
-                    result = results[i];
+                    // Filter seeders : If torrent seeders <= config seeders reject the torrent
+                    if (torrent.seeders <= config.thepiratebay.filters.seeders) {
+                        console.log("-- Torrent reject : Seeders too small (" + torrent.seeders + ")");
+                        favorite_torrent = null;
+                    }
+
+                    // Filter leechers : If torrent leechers <= config leechers reject the torrent
+                    if (torrent.leechers <= config.thepiratebay.filters.leechers) {
+                        console.log("-- Torrent reject : Leechers too small (" + torrent.leechers + ")");
+                        favorite_torrent = null;
+                    }
+
+                    // Filter uploadDate : 12-09 2014
+                    if (config.thepiratebay.filters.uploadDate) {
+                        var torrentDate = new Date();
+                        var torrentDateArray = torrent.uploadDate.split(" ").split("-");
+                        torrentDate.setFullYear(torrentDateArray[2], torrentDateArray[1], torrentDateArray[0]);
+
+                        var filterDate = Date.parse(config.thepiratebay.filters.uploadDate);
+
+                        console.log("-- Check date parsing :");
+                        console.log("---- Torent : " + torrent.uploadDate + " = " + torrentDate.getDay() + torrentDate.getMonth() + torrentDate.getFullYear());
+                        console.log("---- Filter : " + filterDate);
+
+                        // Compare millisconds
+                        if (torrent.uploadDate.getTime() < filterDate) {
+                            favorite_torrent = null;
+                        }
+                    }
+
+                    // If favorite torrent is not null send him
+                    if (favorite_torrent) {
+                        res.send(favorite_torrent);
+                    }
                 }
             }
-
-            res.send(result);
+            res.send(null);
         })
         .catch(function () {
             console.error(err);
--- app/routes/transmission.js
+++ app/routes/transmission.js
@@ -2,19 +2,66 @@ var Transmission = require('transmission');
 var fs = require('fs');
 var ini = require('ini');
 
+// Open config.ini
+var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+var transmission = new Transmission({
+    host: config.transmission.address,      // 192.168.0.21
+    port: config.transmission.port,         // 9091
+    username: config.transmission.username, // wifsimster
+    password: config.transmission.password, // 192lucie
+    url: config.transmission.rpc_url        // /transmission/rpc
+});
+
+function getTorrent(id) {
+    transmission.get(id, function (err, result) {
+        if (err) {
+            throw err;
+        }
+        console.log('bt.get returned ' + result.torrents.length + ' torrents');
+        result.torrents.forEach(function (torrent) {
+            console.log('hashString', torrent.hashString);
+        });
+    });
+}
+
+function removeTorrent(id) {
+    transmission.remove(id, function (err) {
+        if (err) {
+            throw err;
+        }
+        console.log('Torrent was removed !');
+    });
+}
+
+function addTorrent(url, downloadDirectory) {
+    transmission.addUrl(url, {
+        "download-dir": downloadDirectory
+    }, function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        var id = result.id;
+        console.log('New torrent added - ID: ' + id);
+    });
+}
+
+function addTorrent(url) {
+    transmission.addUrl(url, function (err, result) {
+        if (err) {
+            return console.log(err);
+        }
+        var id = result.id;
+        console.log('New torrent added - ID: ' + id);
+    });
+}
+
+// -----------------------------------------------------
+// ----                 ROUTES                      ----
+// -----------------------------------------------------
+
 // Check if Transmission is OK
 app.get("/transmission/test", function (req, res) {
-
-    // Open config.ini
-    var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-
-    var transmission = new Transmission({
-        host: config.transmission.address,      // 192.168.0.21
-        port: config.transmission.port,         // 9091
-        username: config.transmission.username, // wifsimster
-        password: config.transmission.password, // 192lucie
-        url: config.transmission.rpc_url        // /transmission/rpc
-    });
 
     transmission.get(function (err, data) {
         if (!err) {
@@ -40,26 +87,11 @@ app.post("/transmission/add", function (req, res) {
 
     var url = req.body.url;
 
-    // Open config.ini
-    var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
-
-    var transmission = new Transmission({
-        host: config.transmission.address,      // 192.168.0.21
-        port: config.transmission.port,         // 9091
-        username: config.transmission.username, // wifsimster
-        password: config.transmission.password, // 192lucie
-        url: config.transmission.rpc_url        // /transmission/rpc
-    });
-
     console.log("Try to add '" + url + "' to Transmission...");
 
-    transmission.addUrl(url, function (err, arg) {
-        if(!err) {
-            console.log(url + "' added to Transmission");
-            res.send(true);
-        }
+    var result = addTorrent(url);
 
-        console.error(err);
-        res.send(err);
-    })
+    console.log(result);
+
+    res.send(result);
 });
