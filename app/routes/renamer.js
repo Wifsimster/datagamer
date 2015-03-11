ed762a7829593e0aa29ce1c99efd00fee9574a42
Wifsimster <wifsimster@gmail.com>
2015-03-11 09:02:16 +0100

    Refactoring renamer parse filename.

--- app/routes/renamer.js
+++ app/routes/renamer.js
@@ -28,254 +28,243 @@ app.get("/renamer/games/postprocessing", function (req, res) {
     });
 });
 
+// Parse torrent name from provider
+app.get("/renamer/parse/:name", function (req, res) {
+    CODE.SUCCESS.game = parseNameToGame(req.params.name);
+    res.json(CODE.SUCCESS);
+});
+
+// Parse a torrent filename to a complete object Game
+function parseNameToGame(name) {
+    collection_db.loadDatabase();
+    var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
+
+    var game = {};
+
+    //---------------------------------------------------------------------
+    //----           CLEAN THE GAME NAME AND EXTRACT DATA              ----
+    //---------------------------------------------------------------------
+
+    // Extract filename from path
+    var filename = name.split(/(\\|\/)/g).pop();
+
+    winston.info('Renamer --------------------------------------------');
+    winston.info('Renamer - Original filename : ' + filename);
+
+    // Delete " PC " from filename
+    filename = filename.replace(/PC/g, '');
+
+    // Delete " by " from filename
+    filename = filename.replace(/by/g, '');
+
+    // Replace "_" by escape
+    filename = filename.replace(/_/g, ' ');
+
+    // Detect potential crack
+    // Crackfix
+    var crack_regex = /(crack)/i;
+    var crack = crack_regex.exec(filename);
+    if (crack) {
+        game.crack = true;
+        winston.info("Renamer --- With crack");
+        filename = filename.replace(crack_regex, '').pop();
+        //winston.info("Renamer --- Filename after crack : " + filename);
+    }
+
+    // Detect potential repack
+    // RePack
+    var repack_regex = /(repack)/i;
+    var repack = repack_regex.exec(filename);
+    if (repack) {
+        game.repack = true;
+        winston.info("Renamer --- This is a repack");
+        filename = filename.replace(repack_regex, '').pop();
+        //winston.info("Renamer --- Filename after repack : " + filename);
+    }
+
+    // Detect potential multilangue
+    // MULTi
+    var multi_regex = /(multi)/i;
+    var multi = multi_regex.exec(filename);
+    if (multi) {
+        game.multi = true;
+        winston.info("Renamer --- Multilangue");
+        filename = filename.replace(multi_regex, '').pop();
+        //winston.info("Renamer --- Filename after multi : " + filename);
+    }
+
+    // Detect potential release date
+    // (yyyy)
+    // Do it before version detection
+    var release_regex = /(\d{4})/;
+    var releaseDate = release_regex.exec(filename);
+    if (releaseDate) {
+        releaseDate = releaseDate[1];
+        game.releaseDate = releaseDate;
+        winston.info("Renamer --- Release date : " + releaseDate);
+        filename = filename.replace(release_regex, '');
+        //winston.info("Renamer --- Filename after release date : " + filename);
+    }
+
+    // Detect potential version
+    // 1.20.14
+    var version_regex = /(\d+[[\.\d+]+]*)/;
+    var version = version_regex.exec(filename);
+    if (version) {
+        version = version[1];
+        game.version = version;
+        winston.info("Renamer --- Version : " + version);
+        filename = filename.replace(version_regex, '');
+        //winston.info("Renamer --- Filename after version : " + filename);
+    }
+
+    // Detect potential language
+    // [ENG]
+    var language = /\[([A-Z]{3})\]/.exec(filename);
+    if (language) {
+        language = language[1];
+        game.language = language;
+        winston.info("Renamer --- Langague : " + language);
+        filename = filename.replace(/\[([A-Z]{3})\]/, '');
+        //winston.info("Renamer --- Filename after language : " + filename);
+    }
+
+    // Detect potential team
+    // -CODEX
+    var team_regex_1 = /-(CODEX|RELOADED|FLT|R.G.Mechanics|P2P|Razor1911|SKIDROW|STEAMGAMES|GoG|XaTaB|HI2U|PROPHET|WWW|TiNYiSO|TeRM!NaToR|AnCiENT)/;
+    var team_regex_2 = /(\^\^nosTEAM\^\^|TeamExtremeMc\.com|TeRMiNaToR|TeRM!NaToR)/;
+
+    var team = team_regex_1.exec(filename);
+    if (team) {
+        team = team[1];
+        winston.info("Renamer --- Team : " + team);
+        filename = filename.replace(team_regex_1, '');
+    } else {
+        team = team_regex_2.exec(filename);
+        if (team) {
+            team = team[1];
+            game.team = team;
+            winston.info("Renamer --- Team : " + team);
+            filename = filename.replace(team_regex_2, '');
+        }
+    }
+
+    // Delete dots after version detection
+    filename = filename.replace(/\./, ' ');
+
+    // Delete "()" after release date detection
+    filename = filename.replace(/\(\)/, '');
+
+    // Delete useless spaces
+    filename = filename.trim();
+
+    winston.info("Renamer --- Searching for " + filename + " on Datagamer...");
+
+    // Search current video game on Datagamer
+    request('http://localhost:' + config.general.port + '/datagamer/games/similar/' + filename, {
+        headers: {
+            "apiKey": config.search.datagamer.apikey
+        }
+    }, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+            var result = JSON.parse(body);
+
+            if (result.code == 200) {
+                winston.info('Renamer --- Game found on Datagamer : ' + result.games.length);
+
+                if (result.games.length > 0) {
+                    var bestGame = {};
+                    bestGame.percentage = 0;
+
+                    for (var j = 0; j < result.games.length; j++) {
+
+                        //winston.info('Renamer ----- ' + result.games[j].defaultTitle + ' - ' + result.games[j].percentage);
+
+                        if (result.games[j].percentage > bestGame.percentage) {
+                            bestGame = result.games[j];
+                        }
+                    }
+
+                    // Take highest score and rename the file
+                    winston.info('Renamer --- Highest similar game found : ' + bestGame.defaultTitle + ' - ' + bestGame.percentage);
+
+                    // Info from Datagamer
+                    game.datagamer_id = bestGame._id;
+                    game.name = bestGame.defaultTitle;
+                    game.overview = bestGame.overview;
+                    game.percentage = bestGame.percentage;
+                    game.media = {};
+                    if (bestGame.media) {
+                        game.media.thumbnails = bestGame.media.thumbnails;
+                    }
+                    game.platforms = bestGame.platforms;
+                    game.genres = bestGame.genres;
+                    game.developers = bestGame.developers;
+                    game.editors = bestGame.editors;
+                    game.score = bestGame.score;
+                    if (bestGame.releaseDates) {
+                        game.releaseDate = bestGame.releaseDates[0].date;
+                    }
+
+                    return game;
+                }
+
+                return null;
+            }
+
+            return null;
+        }
+
+        return null;
+    });
+}
+
 // Recursive rename
 function recursiveRename(i, config, files, callback) {
     if (i < files.length) {
 
         var file = files[i];
 
-        // Detect .iso file
-        //var regex = /.*\.((iso)$)/;
+        var collectionGame = parseNameToGame(file);
 
-        // Detect a file ended by .iso
-        //if (file.match(regex)) {
-
-        var game = {};
-
-        //---------------------------------------------------------------------
-        //----           CLEAN THE GAME NAME AND EXTRACT DATA              ----
-        //---------------------------------------------------------------------
-
-        // Extract filename from path
-        var filename = file.split(/(\\|\/)/g).pop();
-
-        winston.info('Renamer --------------------------------------------');
-        winston.info('Renamer - Original filename : ' + filename);
-
-        // Delete " PC " from filename
-        filename = filename.replace(/PC/g, '');
-
-        // Delete " by " from filename
-        filename = filename.replace(/by/g, '');
-
-        // Replace "_" by escape
-        filename = filename.replace(/_/g, ' ');
-
-        // Detect potential crack
-        // Crackfix
-        var crack_regex = /(crack)/i;
-        var crack = crack_regex.exec(filename);
-        if (crack) {
-            game.crack = true;
-            winston.info("Renamer --- With crack");
-            filename = filename.replace(crack_regex, '').pop();
-            //winston.info("Renamer --- Filename after crack : " + filename);
-        }
-
-        // Detect potential repack
-        // RePack
-        var repack_regex = /(repack)/i;
-        var repack = repack_regex.exec(filename);
-        if (repack) {
-            game.repack = true;
-            winston.info("Renamer --- This is a repack");
-            filename = filename.replace(repack_regex, '').pop();
-            //winston.info("Renamer --- Filename after repack : " + filename);
-        }
-
-        // Detect potential multilangue
-        // MULTi
-        var multi_regex = /(multi)/i;
-        var multi = multi_regex.exec(filename);
-        if (multi) {
-            game.multi = true;
-            winston.info("Renamer --- Multilangue");
-            filename = filename.replace(multi_regex, '').pop();
-            //winston.info("Renamer --- Filename after multi : " + filename);
-        }
-
-        // Detect potential release date
-        // (yyyy)
-        // Do it before version detection
-        var release_regex = /(\d{4})/;
-        var releaseDate = release_regex.exec(filename);
-        if (releaseDate) {
-            releaseDate = releaseDate[1];
-            game.releaseDate = releaseDate;
-            winston.info("Renamer --- Release date : " + releaseDate);
-            filename = filename.replace(release_regex, '');
-            //winston.info("Renamer --- Filename after release date : " + filename);
-        }
-
-        // Detect potential version
-        // 1.20.14
-        var version_regex = /(\d+[[\.\d+]+]*)/;
-        var version = version_regex.exec(filename);
-        if (version) {
-            version = version[1];
-            game.version = version;
-            winston.info("Renamer --- Version : " + version);
-            filename = filename.replace(version_regex, '');
-            //winston.info("Renamer --- Filename after version : " + filename);
-        }
-
-        // Detect potential language
-        // [ENG]
-        var language = /\[([A-Z]{3})\]/.exec(filename);
-        if (language) {
-            language = language[1];
-            game.language = language;
-            winston.info("Renamer --- Langague : " + language);
-            filename = filename.replace(/\[([A-Z]{3})\]/, '');
-            //winston.info("Renamer --- Filename after language : " + filename);
-        }
-
-        // Detect potential team
-        // -CODEX
-        var team_regex_1 = /-(CODEX|RELOADED|FLT|R.G.Mechanics|P2P|Razor1911|SKIDROW|STEAMGAMES|GoG|XaTaB|HI2U|PROPHET|WWW|TiNYiSO|TeRM!NaToR|AnCiENT)/;
-        var team_regex_2 = /(\^\^nosTEAM\^\^|TeamExtremeMc\.com|TeRMiNaToR|TeRM!NaToR)/;
-
-        var team = team_regex_1.exec(filename);
-        if (team) {
-            team = team[1];
-            winston.info("Renamer --- Team : " + team);
-            filename = filename.replace(team_regex_1, '');
-        } else {
-            team = team_regex_2.exec(filename);
-            if (team) {
-                team = team[1];
-                game.team = team;
-                winston.info("Renamer --- Team : " + team);
-                filename = filename.replace(team_regex_2, '');
-            }
-        }
-
-        // Delete dots after version detection
-        filename = filename.replace(/\./, ' ');
-
-        // Delete "()" after release date detection
-        filename = filename.replace(/\(\)/, '');
-
-        // Delete useless spaces
-        filename = filename.trim();
-
-        winston.info("Renamer --- Searching for " + filename + " on Datagamer...");
-
-        // Search current video game on Datagamer
-        request('http://localhost:' + config.general.port + '/datagamer/games/similar/' + filename, {
-            headers: {
-                "apiKey": config.search.datagamer.apikey
-            }
-        }, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-
-                var result = JSON.parse(body);
-
-                if (result.code == 200) {
-                    winston.info('Renamer --- Game found on Datagamer : ' + result.games.length);
-
-                    if (result.games.length > 0) {
-                        var bestGame = {};
-                        bestGame.percentage = 0;
-
-                        for (var j = 0; j < result.games.length; j++) {
-
-                            //winston.info('Renamer ----- ' + result.games[j].defaultTitle + ' - ' + result.games[j].percentage);
-
-                            if (result.games[j].percentage > bestGame.percentage) {
-                                bestGame = result.games[j];
-                            }
+        collection_db.find({name: collectionGame.name}, function (err, doc) {
+            if (!err) {
+                // If no game with this name exist in collection database
+                if (doc.length == 0) {
+                    // Add this new game to the collection database
+                    collection_db.insert(collectionGame, function (err) {
+                        if (err) {
+                            winston.error(err);
+                            callback();
+                        } else {
+                            winston.info('Collection game added !');
+                            recursiveRename(i + 1, config, files, callback);
                         }
-
-                        // Take highest score and rename the file
-                        winston.info('Renamer --- Highest similar game found : ' + bestGame.defaultTitle + ' - ' + bestGame.percentage);
-
-                        // Add this game to collection database
-                        var collectionGame = {};
-
-                        // Info from torrent
-                        if (releaseDate) {
-                            collectionGame.releaseDate = releaseDate;
-                        }
-                        collectionGame.team = team;
-                        collectionGame.version = version;
-                        collectionGame.language = language;
-                        collectionGame.multi = multi;
-                        collectionGame.repack = repack;
-                        collectionGame.crack = crack;
-
-                        // Info from Datagamer
-                        collectionGame.datagamer_id = bestGame._id;
-                        collectionGame.name = bestGame.defaultTitle;
-                        collectionGame.overview = bestGame.overview;
-                        collectionGame.percentage = bestGame.percentage;
-                        collectionGame.media = {};
-                        if (bestGame.media) {
-                            collectionGame.media.thumbnails = bestGame.media.thumbnails;
-                        }
-                        collectionGame.platforms = bestGame.platforms;
-                        collectionGame.genres = bestGame.genres;
-                        collectionGame.developers = bestGame.developers;
-                        collectionGame.editors = bestGame.editors;
-                        collectionGame.score = bestGame.score;
-                        if (bestGame.releaseDates) {
-                            collectionGame.releaseDate = bestGame.releaseDates[0].date;
-                        }
-
-                        collection_db.find({name: collectionGame.name}, function (err, doc) {
-                            if (!err) {
-                                // If no game with this name exist in collection database
-                                if (doc.length == 0) {
-                                    // Add this new game to the collection database
-                                    collection_db.insert(collectionGame, function (err) {
-                                        if (err) {
-                                            winston.error(err);
-                                            callback();
-                                        } else {
-                                            winston.info('Collection game added !');
-                                            recursiveRename(i + 1, config, files, callback);
-                                        }
-                                    });
-                                } else {
-                                    winston.info(collectionGame.name + ' already exist in collection database !');
-                                    recursiveRename(i + 1, config, files, callback);
-                                }
-                            } else {
-                                winston.error(err);
-                                callback();
-                            }
-                        });
-
-                        // WINDOWS FIX, TO DELETE FOR UNIX USERS
-                        //bestGame.name.replace(/:/,' ');
-
-                        //var directory = config.renamer.to + '/' + collectionGame.name + ' (' + new Date(collectionGame.releaseDate).getFullYear() + ')';
-                        //winston.info('Renamer --- Try to create : ' + directory);
-
-                        //try {
-                        //    fs.mkdir(directory, 0777, function (err) {
-                        //        if (err)
-                        //            console.error(err);
-                        //    });
-                        //} catch (e) {
-                        //    if (e.code != 'EEXIST') console.error(e);
-                        //}
-                    } else {
-                        winston.info('Renamer - No game found on Datagamer for : ' + filename);
-                        recursiveRename(i + 1, config, files, callback);
-                    }
+                    });
                 } else {
-                    winston.error('Renamer - No game found on Datagamer for : ' + filename);
+                    winston.info(collectionGame.name + ' already exist in collection database !');
                     recursiveRename(i + 1, config, files, callback);
                 }
             } else {
-                winston.error('Renamer - No game found on Datagamer for : ' + filename);
-                recursiveRename(i + 1, config, files, callback);
+                winston.error(err);
+                callback();
             }
         });
-        //} else {
-        //    //winston.info('Collection - ' + file + ' is not an .iso, next !');
-        //    recursiveRename(i + 1, config, files, callback);
+
+        // WINDOWS FIX, TO DELETE FOR UNIX USERS
+        //bestGame.name.replace(/:/,' ');
+
+        //var directory = config.renamer.to + '/' + collectionGame.name + ' (' + new Date(collectionGame.releaseDate).getFullYear() + ')';
+        //winston.info('Renamer --- Try to create : ' + directory);
+
+        //try {
+        //    fs.mkdir(directory, 0777, function (err) {
+        //        if (err)
+        //            console.error(err);
+        //    });
+        //} catch (e) {
+        //    if (e.code != 'EEXIST') console.error(e);
         //}
     } else {
         callback();
--- public/js/controllers/home.js
+++ public/js/controllers/home.js
@@ -7,6 +7,10 @@ app.controller('HomeCtrl', function ($scope, $http, LxProgressService, LxNotific
         success(function (result) {
             if (result.code == 200) {
                 $scope.torrents = result.torrents;
+
+                if(result.torrents.length < 1) {
+                    LxNotificationService.warning("No data for the top 10 !");
+                }
             } else {
                 LxNotificationService.error(result.message);
             }
@@ -16,6 +20,20 @@ app.controller('HomeCtrl', function ($scope, $http, LxProgressService, LxNotific
             LxNotificationService.error(err);
             LxProgressService.circular.hide();
         });
+
+    $scope.parseTorrent = function (name) {
+        $http.get('renamer/parse/' + name).
+            success(function (res) {
+                if (res.code == 200) {
+                    console.log(res.game);
+                } else {
+                    LxNotificationService.error(res.message);
+                }
+            }).
+            error(function (err) {
+                LxNotificationService.error(err);
+            });
+    }
 
     $scope.add2Transmission = function (magnetLink) {
         $http.post('/transmission/add', {url: magnetLink}).
