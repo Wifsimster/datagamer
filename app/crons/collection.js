ec560c593d37b320532ae31adf0260894f8c28d9
Wifsimster <wifsimster@gmail.com>
2015-03-12 16:55:48 +0100

    CRON - Post-processing & collection update : Done !

--- app/crons/collection.js
+++ app/crons/collection.js
@@ -5,22 +5,22 @@ var CronJob = require('cron').CronJob;
 var winston = require('winston');
 
 module.exports.start = function () {
-// Open conf file
+    // Open conf file
     var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
-// Collection cron
+    // Collection cron
     if (config.collection.scan_auto) {
 
         // CRON conf
         var collection_cron = config.collection.cron.minute + ' ' + config.collection.cron.hour + ' ' + config.collection.cron.day;
-        winston.info("-- Collection : " + collection_cron);
+        winston.info("CRON - Collection : " + collection_cron);
 
         new CronJob('* ' + collection_cron + ' * * *', function () {
-            winston.info('CRON - Collection :');
-
-            request('http://localhost:' + config.general.port + '/collection/games/scan', function (error, response, body) {
+            request('http://localhost:' + config.general.port + '/renamer/games/postprocessing', function (error, response, body) {
                 if (!error && response.statusCode == 200) {
-                    winston.info("200");
+                    winston.info("CRON - Collection : Post processing files done !");
+                } else {
+                    winston.error(error);
                 }
             });
         }, null, true, "Europe/Paris");
--- app/routes/collection.js
+++ app/routes/collection.js
@@ -14,129 +14,19 @@ app.get("/collection/games/scan", function (req, res) {
     collection_db.loadDatabase();
     var config = ini.parse(fs.readFileSync('./config.ini', 'utf-8'));
 
-    // TODO : Need to detect network or not
+    winston.info("Collection - Scan : " + config.collection.directory);
 
-    winston.info("Collection - Scan \\" + config.collection.directory);
+    var files = getFiles(config.collection.directory);
 
-    var files = getFiles("\\" + config.collection.directory);
-
-    addGamesRecursive(0, config, files, function () {
-        winston.info('Scan ended !')
-        res.send();
-    });
 });
 
-// Recursive loop with callback to process correctly each filename
-function addGamesRecursive(i, config, files, callback) {
-    if (i < files.length) {
-
-        var file = files[i];
-
-        var regex = /.*\.((iso)$)/;
-
-        // Detect a file ended by .iso
-        if (file.match(regex)) {
-            winston.info("Collection - Potential video game file : " + file);
-
-            // Extract filename (no path, no format, no dot)
-            var filename = file.split(/(\\|\/)/g).pop();
-            filename = filename.split('.').reverse().pop();
-
-            var game = {};
-            game.name = filename;
-
-            collection_db.findOne({name: filename}, function (err, result) {
-
-                if (!err) {
-                    if (result) {
-                        winston.info('Collection - ' + result.name + ' already exist !');
-                        addGamesRecursive(i + 1, config, files, callback);
-                    } else {
-                        // First of all add the game in the collection db
-                        collection_db.insert(game, function (err, newDoc) {
-                            if (!err) {
-                                winston.info("Collection - Add " + filename + " to collection database !");
-                                winston.info("Collection - Searching a game for : " + filename);
-
-                                // Search current video game on Datagamer
-                                request('http://localhost:' + config.general.port + '/datagamer/search/' + filename, {
-                                        headers: {
-                                            "apiKey": config.search.datagamer.apikey
-                                        }
-                                    }, function (error, response, body) {
-                                        if (!error && response.statusCode == 200) {
-
-                                            var result = JSON.parse(body);
-
-                                            if (result.code == 200) {
-
-                                                // First game found
-                                                var game = result.games[0];
-
-                                                winston.info("Collection - Find '" + game.name + "' game !");
-
-                                                collection_db.findOne({name: game.name}, function (err, newDoc) {
-                                                    if (!err) {
-                                                        if (newDoc) {
-                                                            winston.info("Collection - Game already in collection, updating the game info...");
-                                                            collection_db.update({name: newDoc.name}, game, {}, function (err, newDoc) {
-                                                                //if (!err)
-                                                                //res.json({message: "OK"});
-                                                            });
-                                                        } else {
-                                                            winston.info("Collection - Game added to collection !");
-
-                                                            // Set the game as downloaded
-                                                            game.downloaded = true;
-
-                                                            collection_db.insert(game, function (err, newDoc) {
-                                                                //if (!err)
-                                                                //res.json({message: "OK"});
-                                                            });
-                                                        }
-                                                    } else {
-                                                        console.error(err);
-                                                        addGamesRecursive(i + 1, config, files, callback);
-                                                    }
-                                                });
-                                            } else {
-                                                console.info("Collection - " + filename + " not found in Datagamer database !");
-                                                console.info("Collection - Need to propose a near game names...");
-
-                                                addGamesRecursive(i + 1, config, files, callback);
-                                            }
-                                        } else {
-                                            console.error("Collection - " + error);
-                                            addGamesRecursive(i + 1, config, files, callback);
-                                        }
-                                    }
-                                );
-                            } else {
-                                console.error(err);
-                                addGamesRecursive(i + 1, config, files, callback);
-                            }
-                        });
-                    }
-                } else {
-                    console.error(err);
-                    addGamesRecursive(i + 1, config, files, callback);
-                }
-            });
-        } else {
-            //winston.info('Collection - ' + file + ' is not an .iso, next !');
-            addGamesRecursive(i + 1, config, files, callback);
-        }
-    } else {
-        callback();
-    }
-}
 
 app.get("/collection/games", function (req, res) {
     collection_db.loadDatabase();
     //winston.info("Getting collection video games...");
     collection_db.find({}, function (err, games) {
         CODE.SUCCESS.games = games;
-        res.send(CODE.SUCCESS);
+        res.json(CODE.SUCCESS);
     });
 });
 
@@ -202,7 +92,7 @@ function getFiles(dir, files_) {
             }
         }
     } catch (err) {
-        console.error(err);
+        winston.error(err);
     }
     return files_;
 }
